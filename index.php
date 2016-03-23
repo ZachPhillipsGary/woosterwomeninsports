76cd45d515e4b75232d2fc3b8aa94eaf39a2c667
Zachary Phillips-Gary <zpg@Zachs-MacBook-Pro.local>
2016-03-23 09:52:28 -0400

    added backend interface for editing  homepage Jumbotron sections

--- index.php
+++ index.php
@@ -32,9 +32,9 @@
   }
 ?>
 <div class="container-fluid highlight-jumbotron">
-
+<!-- section 1 -->
 <div class="row">
-    <div class="col-md-4 col-sm-6">
+    <div class="col-md-4 col-sm-6 front-highlights">
       <div class="section">
         <?php if (!empty($sectionBackgroundImages[0])): ?>
           <!-- do a hacky fix for now -->
@@ -55,7 +55,8 @@
         </div>
       </div>
    </div>
-   <div class="col-md-4 col-sm-6">
+   <!-- section 2 -->
+   <div class="col-md-4 col-sm-6 front-highlights">
      <div class="section">
        <?php if (!empty($sectionBackgroundImages[1])): ?>
          <!-- do a hacky fix for now -->
@@ -71,50 +72,52 @@
          </div>
        </div>
        <div class="back">
-         <p><?php echo $sectionInfos[0] ?></p>
-         <a href="<?php echo $sectionLinks[0] ?>">LEARN MORE</a>
+         <p><?php echo $sectionInfos[1] ?></p>
+         <a href="<?php echo $sectionLinks[1] ?>">LEARN MORE</a>
        </div>
      </div>
    </div>
-   <div class="col-md-4 col-sm-6">
+   <!-- section 3 -->
+   <div class="col-md-4 col-sm-6 front-highlights">
      <div class="section">
-       <?php if (!empty($sectionBackgroundImages[0])): ?>
+       <?php if (!empty($sectionBackgroundImages[2])): ?>
          <!-- do a hacky fix for now -->
-         <img src="<?php echo "./".$themeUploadsPath.$sectionBackgroundImages[0] ?>" class="background">
+         <img src="<?php echo "./".$themeUploadsPath.$sectionBackgroundImages[2] ?>" class="background">
        <?php else: ?>
          <div class="background"></div>
        <?php endif ?>
        <div class="front">
          <div class="container">
-           <span class="glyphicon <?php echo $sectionTitleIcons[0] ?>" aria-hidden="true"></span>
-           <p class="sans-serif-800"><?php echo $sectionTitle1s[0] ?></p>
-           <p class="serif-400"><?php echo $sectionTitle2s[0] ?></p>
+           <span class="glyphicon <?php echo $sectionTitleIcons[2] ?>" aria-hidden="true"></span>
+           <p class="sans-serif-800"><?php echo $sectionTitle1s[2] ?></p>
+           <p class="serif-400"><?php echo $sectionTitle2s[2] ?></p>
          </div>
        </div>
        <div class="back">
-         <p><?php echo $sectionInfos[0] ?></p>
-         <a href="<?php echo $sectionLinks[0] ?>">LEARN MORE</a>
+         <p><?php echo $sectionInfos[2] ?></p>
+         <a href="<?php echo $sectionLinks[2] ?>">LEARN MORE</a>
        </div>
      </div>
    </div>
+   <!-- section 4 -->
    <div class="col-md-4 col-sm-6">
      <div class="section">
-       <?php if (!empty($sectionBackgroundImages[0])): ?>
+       <?php if (!empty($sectionBackgroundImages[3])): ?>
          <!-- do a hacky fix for now -->
-         <img src="<?php echo "./".$themeUploadsPath.$sectionBackgroundImages[0] ?>" class="background">
+         <img src="<?php echo "./".$themeUploadsPath.$sectionBackgroundImages[3] ?>" class="background">
        <?php else: ?>
          <div class="background"></div>
        <?php endif ?>
        <div class="front">
          <div class="container">
-           <span class="glyphicon <?php echo $sectionTitleIcons[0] ?>" aria-hidden="true"></span>
-           <p class="sans-serif-800"><?php echo $sectionTitle1s[0] ?></p>
-           <p class="serif-400"><?php echo $sectionTitle2s[0] ?></p>
+           <span class="glyphicon <?php echo $sectionTitleIcons[3] ?>" aria-hidden="true"></span>
+           <p class="sans-serif-800"><?php echo $sectionTitle1s[3] ?></p>
+           <p class="serif-400"><?php echo $sectionTitle2s[3] ?></p>
          </div>
        </div>
        <div class="back">
-         <p><?php echo $sectionInfos[0] ?></p>
-         <a href="<?php echo $sectionLinks[0] ?>">LEARN MORE</a>
+         <p><?php echo $sectionInfos[3] ?></p>
+         <a href="<?php echo $sectionLinks[3] ?>">LEARN MORE</a>
        </div>
      </div>
    </div>

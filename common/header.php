c99403085c7a0f87eb3b9932be70fe3ab9d81e3a
Zachary Phillips-Gary <zpg@Zachs-MacBook-Pro.local>
2016-03-24 12:16:24 -0400

    affix main menu

--- common/header.php
+++ common/header.php
@@ -73,7 +73,10 @@
       </div><!-- end of container-->
     </div><!-- end of nav-container-->
   </div><!-- end of nav-drawer-->
-  <div class="navbar navbar-inverse ">
+  <div class="container-fluid" style="background-color:#FFCC00;color:#000;height:100px;">
+  <h1>The College of Wooster</h1>
+  </div>
+  <div data-spy="affix" data-offset-top="197" class="navbar navbar-inverse ">
   <div class="container-fluid">
     <div class="hidden-xs hidden-xs navbar-header collapse navbar-collapse">
       <a class="navbar-brand" href="../">Women in Sport and Physical Education</a>

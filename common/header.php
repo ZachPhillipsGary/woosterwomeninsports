6e1f9adf18d045e6b96b3ccf0d942ab5728d1dd3
Zachary Phillips-Gary <zpg@Zachs-MacBook-Pro.local>
2016-03-31 11:15:56 -0400

    fixed homepage link

--- common/header.php
+++ common/header.php
@@ -74,7 +74,7 @@
     </div><!-- end of nav-container-->
   </div><!-- end of nav-drawer-->
   <div class="container-fluid" style="background-color:#FFCC00;color:#000;height:100px;">
-  <h1 id="siteTitleHeader"><a href="#"><?php echo option('site_title'); ?></a></h1>
+  <h1 id="siteTitleHeader"><a href="//104.236.254.210/omeka/"><?php echo option('site_title'); ?></a></h1>
   </div>
   <div data-spy="affix" data-offset-top="197" class="hidden-xs hidden-sm navbar navbar-inverse ">
   <div class="container-fluid">

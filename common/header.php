7e620a0f5906383481051b842399a29d72de937c
Zachary Phillips-Gary <zpg@Zachs-MacBook-Pro.local>
2016-04-07 02:11:38 -0400

    removed color, added hamburger label

--- common/header.php
+++ common/header.php
@@ -106,6 +106,6 @@
 </div>
 
   <label class="hidden-lg hidden-md hidden-xl navigation-button" for="nav-drawer-toggle">
-    
+    MENU
     <span class="glyphicon glyphicon-menu-hamburger" aria-hidden="true"></span>
   </label>

4d8e2669c5c155108bae0f8984effc2d1060e643
Zachary Phillips-Gary <zpg@Zachs-MacBook-Pro.local>
2016-04-19 19:36:47 -0400

    test

--- exhibit-builder/exhibits/show.php
+++ exhibit-builder/exhibits/show.php
@@ -23,8 +23,9 @@
       </div>
     </div>
     <nav id="exhibit-pages" class="col-md-2" >
+    <?php echo exhibit_builder_link_to_exhibit($exhibit); ?>
     <div class="dropdown">
- <a data-target="#" href="page.html" data-toggle="dropdown" class="dropdown-toggle"> <?php echo exhibit_builder_link_to_exhibit($exhibit); ?><b class="caret"></b></a>
+ <a data-target="#" href="page.html" data-toggle="dropdown" class="dropdown-toggle">Test<b class="caret"></b></a>
       <?php $menu = exhibit_builder_page_tree($exhibit, $exhibit_page); 
  $dropdownMenu = preg_replace('/(<ul\b[^><]*)>/i', '$1 class="dropdown-menu">', $menu);
  echo $dropdownMenu;

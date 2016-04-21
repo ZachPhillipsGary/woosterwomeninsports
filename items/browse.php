ba42e7d77ea430ea0c2c697dad9d177510db2725
Zachary Phillips-Gary <zpg@Zachs-MacBook-Pro.local>
2016-04-20 23:03:03 -0400

    tag cloud

--- items/browse.php
+++ items/browse.php
@@ -36,7 +36,8 @@
         </div>
         <div class="modal-body">
           <div id="tagCloud">
-          <?php echo get_recent_tags(99999); ?>
+          <?php $tags = get_recent_tags(99999);
+           echo tag_cloud($tags, 'exhibits/browse'); ?>
               <?php foreach (loop('items') as $item): ?>
               <?php $itemTags = tag_string('item', 'items/browse', ''); ?>
               <div class="cloudtags"><?php //echo $itemTags; ?></div>

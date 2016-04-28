19ad1988cc49936a69587727cb4dce63bd75063f
Zachary Phillips-Gary <zpg@Zachs-MacBook-Pro.local>
2016-04-27 20:16:37 -0400

    test

--- common/header.php
+++ common/header.php
@@ -76,7 +76,7 @@
   <div class="container-fluid" style="background-color:<?php echo get_theme_option('banner_color'); ?>;height:100px;">
   <?php 
     $string = link_to_home_page();
-    $match;
+    print web_path_to(get_theme_option("homepage_logo_large"));
     preg_match('#/<a href=\"([^\"]*)\">(.*)<\/a>/iU#', link_to_home_page(), $match);
     print_r($match);
   ?>

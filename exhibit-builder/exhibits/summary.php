ceb603edd350851cd6da7dbee71c41a1eef793c3
Zachary Phillips-Gary <zpg@Zachs-MacBook-Pro.local>
2016-04-29 00:58:27 -0400

    modified exhibits summary page

--- exhibit-builder/exhibits/summary.php
+++ exhibit-builder/exhibits/summary.php
@@ -8,7 +8,15 @@
   $tags = tag_string('exhibit', 'exhibits', '');
   $pageTree = exhibit_builder_page_tree();
 ?>
+   <div class="dropdown">
 
+    <button class="btn btn-default center-block dropdown-toggle" data-spy="affix" type="button" id="exhibitMenu" data-toggle="dropdown">Menu<span class="caret"></span></button>
+      <?php $menu = exhibit_builder_page_nav(); 
+ $dropdownMenu = preg_replace('/(<ul\b[^><]*)>/i', '$1 role="menu" aria-labelledby="exhibitMenu" class="dropdown-menu">', $menu);
+ echo $dropdownMenu;
+      ?>
+
+</div>
 <div class="container exhibit">
   <div class="section-header col-md-10 col-md-offset-1">
     <small>-EXHIBITS SUMMARY-</small>
@@ -19,6 +27,8 @@
     <div class="col-md-8 col-md-offset-2">
       <div class="article-content">
      <!--   <?php echo exhibit_builder_page_nav(); ?> -->
+
+
         <?php echo $description; ?>
         <b>Credits</b>
         <p><?php echo $credits; ?></p>

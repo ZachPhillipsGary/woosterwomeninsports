6ceceb9bb6f16f3b2085874e8d0b0529bf29390d
Zachary Phillips-Gary <zpg@Zachs-MacBook-Pro.local>
2016-04-29 00:50:30 -0400

    test new menu location

--- exhibit-builder/exhibits/show.php
+++ exhibit-builder/exhibits/show.php
@@ -7,11 +7,6 @@
 <?php
   $title = metadata('exhibit_page', 'title');
 ?>
-<div class="container exhibit">
-
-  <div class="section-header col-md-8 col-md-offset-2">
-   <div class="dropdown">
-
     <button class="btn btn-default center-block dropdown-toggle" data-spy="affix" data-offset-top="197" type="button" id="exhibitMenu" data-toggle="dropdown">Menu<span class="caret"></span></button>
       <?php $menu = exhibit_builder_page_tree($exhibit, $exhibit_page); 
  $dropdownMenu = preg_replace('/(<ul\b[^><]*)>/i', '$1 role="menu" aria-labelledby="exhibitMenu" class="dropdown-menu">', $menu);
@@ -19,6 +14,12 @@
       ?>
 
 </div>
+<div class="container exhibit">
+
+  <div class="section-header col-md-8 col-md-offset-2">
+   <div class="dropdown">
+
+
     <small>-EXHIBIT-</small>
     <h1><?php echo $title ?></h1>
   </div><!-- end of section-header -->
